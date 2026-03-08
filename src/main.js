45ea2503d8987967a75c4ebe86894b0d69a25cce
AtreyuArtax <82472620+AtreyuArtax@users.noreply.github.com>
2026-03-08 10:02:33 -0400

    small fixes

--- src/main.js
+++ src/main.js
@@ -11,5 +11,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/main.css'
 
+async function requestPersistentStorage() {
+    if (navigator.storage && navigator.storage.persist) {
+        const isPersisted = await navigator.storage.persisted()
+        if (!isPersisted) {
+            const granted = await navigator.storage.persist()
+            if (granted) {
+                console.log('Storage will not be cleared except by explicit user action')
+            } else {
+                console.warn('Storage may be cleared by the UA under storage pressure.')
+            }
+        } else {
+            console.log('Storage is already persisted.')
+        }
+    }
+}
+
+requestPersistentStorage()
+
 const app = createApp(App)
 app.mount('#app')
