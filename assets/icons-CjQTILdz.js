557d55907f499b5d9ee04bb701f20f0b7b55645a
AtreyuArtax <82472620+AtreyuArtax@users.noreply.github.com>
2026-04-02 12:48:51 -0400

    Updates

--- assets/icons-CjQTILdz.js
+++
@@ -1,1 +0,0 @@
-import{b1 as o,aR as e}from"./vendor-vYscx3i2.js";function s(r){return o[r]??e}export{s as r};
---
+++ assets/icons-R2LaEiYG.js
@@ -0,0 +1,1 @@
+import{b1 as o,aS as e}from"./vendor-Bv4aeI_a.js";function s(r){return o[r]??e}export{s as r};
