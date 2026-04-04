5a85fd824b587febd5b50606f97c1a13a154f12c
AtreyuArtax <82472620+AtreyuArtax@users.noreply.github.com>
2026-04-04 08:42:44 -0400

    more tidy i hope

--- src/db/classService.js
+++ src/db/classService.js
@@ -500,6 +500,8 @@ export async function clearAllData() {
         capGradesAt100: true,
         gradebookTemplates: [],
         gradebookMilestones: [],
+        academicTerms: [],
+        teacherName: '',
         periodStartTimes: {
             1: '08:45', 2: '10:05', 3: '11:25', 4: '12:45',
             5: '08:45', 6: '10:05', 7: '11:25', 8: '12:45'
